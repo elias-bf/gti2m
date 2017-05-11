9c9c6f7000b489a755a0c3206c4d5262ceca4881
Elias Ferreira <eliasferreira@go.senac.br>
2017-05-11 10:46:27 -0300

    mais comentarios

--- eliasferreira/ola.java
+++ eliasferreira/ola.java
@@ -1,7 +1,12 @@
 public class ola{
 
 	public static void main(String arsg[]){
-		System.out.println("Bom dia")
+		System.out.println("Bom dia");
+		System.out.println("Goiânia, 11 de maio");
+		System.out.println("Faculdade Senac Goias");
+		System.out.println("Laboratorio 311");
+		
+
 	}
 
 }
