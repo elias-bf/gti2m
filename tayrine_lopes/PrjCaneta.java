264f927d9550b3b97a0a37517a763eeafcef17e3
tayrine_lopes <tayrinelguimaraes@gmail.com>
2017-05-11 12:01:04 -0300

    meu dues, deu certo

---
+++ tayrine_lopes/PrjCaneta.java
@@ -0,0 +1,14 @@
+
+package prjcaneta;
+
+
+public class PrjCaneta {
+
+    public static void main(String[] args) 
+    {
+        Caneta c1 = new Caneta();
+        
+        System.out.println(c1.toString()); 
+    }
+    
+}
