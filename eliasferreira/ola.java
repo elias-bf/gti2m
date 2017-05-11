67a0280f0d370d83e2443bbe6f222dcc34f5297e
Manoel Cunha <manoel@cunha.com.br>
2017-05-11 10:45:19 -0300

    alteracao no aruivo java

--- eliasferreira/ola.java
+++ eliasferreira/ola.java
@@ -1,7 +1,10 @@
 public class ola{
 
 	public static void main(String arsg[]){
-		System.out.println("Bom dia")
+		System.out.println("Bom dia");
+		System.out.println("Um texto qualquer");
+		System.out.println("Mais um texto qualquer");
+
 	}
 
 }
